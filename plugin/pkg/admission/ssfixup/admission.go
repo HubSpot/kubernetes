dc98a80608f2fd37623e969d838d3751b8f4d13d
tpetr <tpetr@hubspot.com>
2017-08-14 18:35:22 -0400

    debug logging

--- plugin/pkg/admission/ssfixup/admission.go
+++ plugin/pkg/admission/ssfixup/admission.go
@@ -6,6 +6,7 @@ import (
 	"k8s.io/kubernetes/pkg/api"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	podapi "k8s.io/kubernetes/pkg/api/v1/pod"
+	"github.com/golang/glog"
 )
 
 func init() {
@@ -28,11 +29,20 @@ func (ssf *ssFixup) Admit(attributes admission.Attributes) (err error) {
 		return apierrors.NewBadRequest("Resource was marked with kind Pod but was unable to be converted")
 	}
 
+	glog.V(2).Infof("Got pod %s", pod.Name)
+
+	glog.V(2).Infof("Annotation %s = %s", podapi.PodHostnameAnnotation, pod.Annotations[podapi.PodHostnameAnnotation])
+	glog.V(2).Infof("Annotation %s = %s", podapi.PodSubdomainAnnotation, pod.Annotations[podapi.PodSubdomainAnnotation])
+	glog.V(2).Infof("pod.Spec.Hostname = '%s'", pod.Spec.Hostname)
+	glog.V(2).Infof("pod.Spec.Subdomain = '%s'", pod.Spec.Subdomain)
+
 	if value, ok := pod.Annotations[podapi.PodHostnameAnnotation]; ok && pod.Spec.Hostname == "" {
+		glog.V(2).Infof("Setting pod.Spec.Hostname to '%s'", value)
 		pod.Spec.Hostname = value
 	}
 
 	if value, ok := pod.Annotations[podapi.PodSubdomainAnnotation]; ok && pod.Spec.Subdomain == "" {
+		glog.V(2).Infof("Setting pod.Spec.Subdomain to '%s'", value)
 		pod.Spec.Subdomain = value
 	}
 
